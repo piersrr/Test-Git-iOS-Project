c929ac5f270e4b32ba2364f58e4f578eb9e9fb18
Piers Rudgard-Redsell <piersrr@gmail.com>
2014-03-13 21:14:06 +0000

    added test class

---
+++ PATest.h
@@ -0,0 +1,13 @@
+//
+//  PATest.h
+//  gitTester
+//
+//  Created by Piers Rudgard-Redsell on 13/03/2014.
+//  Copyright (c) 2014 Airbyte. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PATest : NSObject
+
+@end
---
+++ PATest.m
@@ -0,0 +1,13 @@
+//
+//  PATest.m
+//  gitTester
+//
+//  Created by Piers Rudgard-Redsell on 13/03/2014.
+//  Copyright (c) 2014 Airbyte. All rights reserved.
+//
+
+#import "PATest.h"
+
+@implementation PATest
+
+@end
